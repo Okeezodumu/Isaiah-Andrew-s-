a67284ee810d1d9452ccbf23f58d52257c50856c
Okeezodumu <okopiodums@gmail.com>
2023-08-08 15:21:18 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,56 @@
+console.log("javascript ")
+console.log("javascript" + "" + "is cool")
+var mystring = "new to javascript"
+console.log(mystring)
+var firstNum = 500
+var secNum = 200
+
+let result1 = firstNum + secNum
+let result2 = firstNum - secNum
+let result3 = firstNum * secNum
+let result4 = firstNum / secNum
+let result5 = firstNum % secNum
+
+
+console.log("firstNum + secNum = " + "" + result1)
+console.log("firstNum + secNum = " + "" + result2)
+console.log("firstNum + secNum = " + "" + result3)
+console.log("firstNum + secNum = " + "" + result4)
+console.log("firstNum + secNum = " + "" + result5)
+
+var Addition = result1 
+console.log("Addition = " + "" + result1)
+var Subtraction = result2
+console.log("Subtraction = " + "" + result2)
+var Multiplication = result3
+console.log("Multiplication = " + "" + result3)
+var Division = result4
+console.log("Division = " + "" + result4)
+var Modulus = result5
+console.log("Modulus =" + "" + result5)
+
+
+
+
+let myVar = 5
+myVar += 1
+
+console.log(myVar)
+
+let myCar = 5
+myCar + 1
+console.log(myCar)
+
+let myvae = 5
+myvae = + +
+console.log(myvae)
+
+my2Var= 6
+my2Var --
+console.log(my2Var)
+
+myDec = 2.5
+myDec = myDec + 5
+myDec = myDec *2.2
+myDec = myDec % 2
+console.log(myDec)
